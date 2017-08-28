1033aed1011b2156f17d957587945cbeca10cccb
Thomas Dashney <tdashney@gmail.com>
2017-08-27 20:19:42 -0400

    configure jest

---
+++ jest-setup.js
@@ -0,0 +1,16 @@
+const localStorageMock = (function () {
+  let store = {}
+
+  return {
+    getItem: (key) => store[key] || null,
+    setItem: (key, value) => {
+      store[key] = value.toString()
+    },
+    removeItem: (key) => delete store[key],
+    clear: () => { store = {} }
+  }
+})()
+
+Object.defineProperty(window, 'localStorage', {
+  value: localStorageMock
+})
