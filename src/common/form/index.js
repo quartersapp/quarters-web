d2c4b9a171543a872cc57a5348e2e30d81d16750
Thomas Dashney <tdashney@gmail.com>
2017-09-24 02:20:22 -0400

    remove common/form

--- src/common/form/index.js
+++
@@ -1,7 +0,0 @@
-export { default as createForm } from './create-form'
-export { default as connectField } from './connect-field'
-export { default as createFormReducer } from './reducer'
-export {
-  createFormValuesSelector,
-  createFormIsValidSelector
-} from './selectors'
