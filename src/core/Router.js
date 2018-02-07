44d5250c8960a080940132fd281bc4687edf52f0
Thomas Dashney <tdashney@theworkinggroup.ca>
2018-02-07 01:13:55 -0500

    improve redirects between login & signup

--- src/core/Router.js
+++ src/core/Router.js
@@ -8,17 +8,27 @@ import { authenticatedSelector } from 'common/auth'
 import { SignupPage } from 'core/signup'
 import { Redux } from 'common/components'
 
-const RedirectToSignupIfNotAuth = () => (
+const RedirectIfNotAuth = ({ children = null, to = '/signup' }) => (
   <Redux selector={authenticatedSelector}>
-    {({ state: authenticated }) => authenticated ? null : <Redirect to='/signup' />}
+    {({ state: authenticated }) => authenticated ? children : <Redirect to={to} />}
+  </Redux>
+)
+
+const RedirectIfAuth = ({ children, to }) => (
+  <Redux selector={authenticatedSelector}>
+    {({ state: authenticated }) => authenticated ? <Redirect to={to} /> : children}
   </Redux>
 )
 
 const Router = () => (
   <BrowserRouter>
     <div>
-      <Route exact path='/' component={RedirectToSignupIfNotAuth} />
-      <Route path='/signup' component={SignupPage} />
+      <Route exact path='/' component={RedirectIfNotAuth} />
+      <Route exact path='/signup' render={() => (
+        <RedirectIfAuth to='/'>
+          <SignupPage />
+        </RedirectIfAuth>
+      )} />
     </div>
   </BrowserRouter>
 )
