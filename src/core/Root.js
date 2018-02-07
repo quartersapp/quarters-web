5083ed3d2f0dac266c7f95f0f35cbb43d6218b31
Thomas Dashney <tdashney@theworkinggroup.ca>
2018-02-07 01:24:35 -0500

    display current user

--- src/core/Root.js
+++ src/core/Root.js
@@ -1,21 +1,36 @@
 import React from 'react'
+import { Query } from 'react-apollo'
+import gql from 'graphql-tag'
 
 import { authenticatedSelector, logout } from 'common/auth'
 import { LoginForm } from 'core/login-form'
 import Router from './Router'
 import { Redux } from 'common/components'
 
+const currentUserQuery = gql`
+  query CurrentUser {
+    currentUser { email }
+  }
+`
+
 const TempLoginHandler = () => (
   <Redux actions={{ logout }} selector={authenticatedSelector}>
     {({ actions: { logout }, state: authenticated }) => {
       if (authenticated) {
         return (
-          <button type='button' onClick={e => {
-            e.preventDefault()
-            logout()
-          }}>
-            Logout
-          </button>
+          <div>
+            <Query query={currentUserQuery}>
+              {({ data }) => (
+                data ? <div>Hello, {data.currentUser.email}</div> : null
+              )}
+            </Query>
+            <button type='button' onClick={e => {
+              e.preventDefault()
+              logout()
+            }}>
+              Logout
+            </button>
+          </div>
         )
       } else {
         return <LoginForm />
