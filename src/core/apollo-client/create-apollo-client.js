8b81af39f41f7611d861d2c4a3a3d8b72ae6a763
Thomas Dashney <tdashney@theworkinggroup.ca>
2018-02-07 01:31:25 -0500

    add query batching & deduping

--- src/core/apollo-client/create-apollo-client.js
+++ src/core/apollo-client/create-apollo-client.js
@@ -1,12 +1,15 @@
 /* global localStorage */
 
-import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-client-preset'
+import { ApolloClient, InMemoryCache } from 'apollo-client-preset'
 import { setContext } from 'apollo-link-context'
+import { BatchHttpLink } from 'apollo-link-batch-http'
+import { DedupLink } from 'apollo-link-dedup'
 
 import { API_URL } from 'config'
 
 export default () => {
-  const httpLink = new HttpLink({ uri: `${API_URL}/graphql` })
+  const httpLink = new BatchHttpLink({ uri: `${API_URL}/graphql` })
+  const dedupLink = new DedupLink()
 
   const middlewareLink = setContext(() => {
     const headers = {}
@@ -21,7 +24,7 @@ export default () => {
   })
 
   return new ApolloClient({
-    link: middlewareLink.concat(httpLink),
+    link: middlewareLink.concat(dedupLink).concat(httpLink),
     cache: new InMemoryCache()
   })
 }
